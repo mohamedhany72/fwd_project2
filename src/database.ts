e41ca8d339a8f51fd8e9493a75a29fce00fc8370
mohamedhany72 <medocapo304@yahoo.com>
2022-04-16 20:30:25 +0200

    specify db port

--- src/database.ts
+++ src/database.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const {
     POSTGRES_HOST,
     POSTGRES_DB,
+    POSTGRES_PORT,
     POSTGRES_TEST_DB,
     POSTGRES_USER,
     POSTGRES_PASS,
@@ -17,6 +18,7 @@ let client: Pool | undefined;
 if (ENV === "dev") {
     client = new Pool({
         host: POSTGRES_HOST,
+        port: (POSTGRES_PORT as unknown) as number,
         database: POSTGRES_DB,
         user: POSTGRES_USER,
         password: POSTGRES_PASS
@@ -26,6 +28,7 @@ if (ENV === "dev") {
 if (ENV === "test") {
     client = new Pool({
         host: POSTGRES_HOST,
+        port: (POSTGRES_PORT as unknown) as number,
         database: POSTGRES_TEST_DB,
         user: POSTGRES_USER,
         password: POSTGRES_PASS
